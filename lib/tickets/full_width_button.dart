7accc1662dba49e3dce18c869ec568e71c3a6c73
Marc <Marc.dietmann1@gmail.com>
2022-07-04 00:10:48 +0200

    changed architecture

---
+++ lib/tickets/full_width_button.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class FullWidthButton extends StatelessWidget {
+  const FullWidthButton({Key? key,}) : super(key: key);
+
+
+  @override
+  Widget build(BuildContext context) {
+
+    return GestureDetector(
+      onTap: ()=> {},
+      child: Container(),
+    );
+  }
+
+}
+
+
